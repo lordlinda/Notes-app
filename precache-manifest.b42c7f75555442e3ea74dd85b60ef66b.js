67373de62a7d0e6cfb84d0bf70ca55f965238804
lordlinda <marialindananono@gmail.com>
2020-05-02 02:56:44 +0300

    Updates

---
+++ precache-manifest.b42c7f75555442e3ea74dd85b60ef66b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726e2421170c07558e19a7d9314b4fbb",
+    "url": "/Notes-app/index.html"
+  },
+  {
+    "revision": "1aeaec6ac7cdfb359ef3",
+    "url": "/Notes-app/static/css/main.531b3985.chunk.css"
+  },
+  {
+    "revision": "e46f8fbab38275caf48c",
+    "url": "/Notes-app/static/js/2.49cb7bc9.chunk.js"
+  },
+  {
+    "revision": "140497d37cf71cf7e3387cea5cee55b9",
+    "url": "/Notes-app/static/js/2.49cb7bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aeaec6ac7cdfb359ef3",
+    "url": "/Notes-app/static/js/main.24687c99.chunk.js"
+  },
+  {
+    "revision": "f481112a254c25b60982",
+    "url": "/Notes-app/static/js/runtime-main.90df7969.js"
+  }
+]);
